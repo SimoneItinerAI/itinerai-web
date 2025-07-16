025d6649602e35be201c8b0a2f91fb76efea51a6
SimoneItinerAI <simonesammarco01@gmail.com>
2025-07-17 00:21:22 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{ display:'flex',justifyContent:'center',alignItems:'center',height:'100vh',fontFamily:'sans-serif',flexDirection:'column'}}>
+      <h1>Benvenuto nella beta di ItinerAI!</h1>
+      <p>Work in progress…</p>
+    </main>
+  );
+}
