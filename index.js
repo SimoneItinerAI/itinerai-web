e959ce0a37519753beb493e1f93fb218bab3cd3a
SimoneItinerAI <simonesammarco01@gmail.com>
2025-07-17 00:14:26 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main style={{ display:'flex',justifyContent:'center',alignItems:'center',height:'100vh',fontFamily:'sans-serif',flexDirection:'column'}}>
-      <h1>Benvenuto nella beta di ItinerAI!</h1>
-      <p>Work in progress…</p>
-    </main>
-  );
-}
